4068deb913ba4b0ed140e442d5660941c6a197eb
poomwarawat <58084725+poomwarawat@users.noreply.github.com>
2020-02-28 22:53:28 +0700

    setState

--- src/App.js
+++ src/App.js
@@ -1,40 +1,33 @@
 import React, {Component} from 'react';
-import Header from './Header'
 
 class App extends Component {
-    handleClick = (name, ...other) =>{
-        console.log("name => " ,name , "other => ", other)
+    
+    state = {
+        likeme : 0
     }
     render(){
-            
-        //Spread Operator
-        let obj1 = { a:1, b:2}
-        let obj2 = { c:3, d:4}
-        let Result = {...obj1, ...obj2}
-        let arr1 = [1,2]
-        let arr2 = [3,4]
-        console.log(...arr1, ...arr2)
-        console.log(Result)
-        //Rest Parameter
-
-
-        //Destructuring
-        const order = {
-            id : '1',
-            name : 'poom',
-            product : {
-                name1 : "Notebook",
-                price : "25,000"
-            }
-        }
-        const {name ,id, product:{name1, price }} = order
-        console.log(name, id) // เท่ากัน console.log(order.name, order.id)
-        console.log(name, id, name1, price)
+        
         return ( 
             <div className="App">
-                <button onClick={() => this.handleClick("Warawat", "kmutt", "20y.", "ayw")}>Click me</button>
+                <h1>{this.state.likeme}</h1>
+                <button onClick={this.handleClick}>Like</button>
             </div>
         );
+    }
+
+    handleClick = () =>{
+        //แบบนี้ดี
+        this.setState((prevState, props) => {
+            return{
+                likeme : prevState.likeme + 1
+            }
+        })
+
+        //this.state.likeme = 5 // Error
+
+        // this.setState({
+        //     likeme : this.state.likeme + 1 // เกือบถูก
+        // })
     }
 }
 
