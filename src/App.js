add33dd6efef4408faaa23e19b94f48a08e3bd0b
poomwarawat <58084725+poomwarawat@users.noreply.github.com>
2020-02-28 22:20:36 +0700

    ES6 P1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Header from './Header'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            likeme : 1
+        }
+    }
+    handleClick = (name) =>{
+        alert(this.state.likeme + " " + name)
+    }
+    render(){
+        return ( 
+            <div>
+                <Header title="Hello, Poom Warawat"></Header>
+                <button onClick={() => this.handleClick("Hello Penguin")}>Click me</button>
+            </div>
+        );
+    }
 }
 
 export default App;
