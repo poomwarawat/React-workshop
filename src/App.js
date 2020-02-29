af40bea1a4f1ff60d5346d033507b8bb8e15d715
poomwarawat <58084725+poomwarawat@users.noreply.github.com>
2020-02-29 17:18:36 +0700

    Load data json

--- src/App.js
+++ src/App.js
@@ -1,42 +1,49 @@
 import React, {Component} from 'react';
-import LikeButton from './LikeButton'
-import Profile from './Profile'
+
 
 class App extends Component {
-    constructor(){
-        super()
+    constructor(props){
+        super(props)
         this.state = {
-            Like : 0,
-            showButton : true
+            data : null,
+            err : null 
         }
-        console.log("App : constructor")
     }
     componentDidMount(){
-        console.log("App : componentDidMount")
+        fetch("https://jsonplaceholder.typicode.com/posts?userId/1")
+        .then(result =>{
+            if(result.ok){
+                return result
+            }else throw Error(result.status)
+        })
+        .then(result => result.json())
+        .then(result => this.setState((ps) => {return { data: result }}))
+        .catch(e => this.setState({ err : e.message }))
     }
     render(){
-        console.log("App : Render")
+        const {data , err } = this.state
+        let list = <h1>Loading...</h1>
+        if (data) {
+            list = data.map((datas) =>{
+                return(
+                    <div key={datas.id}>
+                        <h3>{datas.title}</h3>
+                        <h2>{datas.body}</h2>
+                    </div>
+                )
+            })
+        }else{
+            list = <h1>No Data</h1>
+        }
+        if(err){
+            list = <h1>{err}</h1>
+        }
         return ( 
             <div className="App">
-                <h1>React L ifecycle</h1>
-                <Profile like={this.state.Like} hideBtn={this.setHidden}></Profile>
-                {this.state.showButton && <LikeButton onClickLike={this.handleClick}></LikeButton>}
+                <h1>Load Data</h1>
+                {list}
             </div>
         );
-    }
-    handleClick = () =>{
-        this.setState((ps) =>{
-            return{
-                Like : ps.Like + 1
-            }
-        })
-    }
-    setHidden = () =>{
-        this.setState((ps) =>{
-            return{
-                showButton : false
-            }
-        })
     }
 }
 
